77c6c88c214b1d17246d89f5c6548bf637ef22cc
Pablo Ahumada <pablo.ahumadadiaz@gmail.com>
2017-06-16 03:45:08 +0000

    Eliminate redundat exceptions

--- relay_api/core/relay.py
+++ relay_api/core/relay.py
@@ -3,7 +3,6 @@ import RPi.GPIO as GPIO
 class relay():
     def __init__(self, gpio_num, relay_type="NO"):
         self.gpio_num = gpio_num
-        self.type = relay_type
         GPIO.setmode(GPIO.BCM)
         try:
             GPIO.input(self.gpio_num)
@@ -12,22 +11,16 @@ class relay():
             GPIO.setup(self.gpio_num, GPIO.OUT)
         except ValueError:
             raise LookupError("Relay number invalid!")
-        if self.type == "NC":
+        if relay_type == "NC":
             self.on()
         else:
             self.off()
 
     def on(self):
-        try:
-            GPIO.output(self.gpio_num, GPIO.HIGH)
-        except RuntimeError:
-            raise LookupError("Relay doesn't exist!")
+        GPIO.output(self.gpio_num, GPIO.HIGH)
 
     def off(self):
-        try:
-            GPIO.output(self.gpio_num, GPIO.LOW)
-        except RuntimeError:
-            raise LookupError("Relay doesn't exist!")
+        GPIO.output(self.gpio_num, GPIO.LOW)
 
     def cleanup(self):
         GPIO.cleanup(self.gpio_num)
