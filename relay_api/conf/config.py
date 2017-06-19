952438d97fc0c96afaf505469cc7b9cb0c9f287d
Pablo Ahumada <pablo.ahumadadiaz@gmail.com>
2017-06-18 22:12:30 -0400

    Add config file with the list of relays availables

---
+++ relay_api/conf/config.py
@@ -0,0 +1,13 @@
+# List of available relays
+relays = [
+    {
+        "id": 1,
+        "gpio": 20,
+        "name": "relay 1"
+    },
+    {
+        "id": 2,
+        "gpio": 21,
+        "name": "relay 2"
+    }
+]
