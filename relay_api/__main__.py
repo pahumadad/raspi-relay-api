f0ede3d2c32d4d3adce98cd3b762b672f7af91a9
Pablo Ahumada <pablo.ahumadadiaz@gmail.com>
2017-06-20 05:29:57 +0000

    Update to generate a dict with the relay instances

--- relay_api/__main__.py
+++ relay_api/__main__.py
@@ -4,16 +4,16 @@ from relay_api.conf.config import relays
 import relay_api.api.server as api
 
 
+relays_dict = {}
 for r in relays:
-    relays[r]["instance"] = relay(relays[r]["gpio"],
-                                  relays[r]["NC"])
+    relays_dict[r] = relay(relays[r]["gpio"], relays[r]["NC"])
 
 
 @server.route("/relay-api/relays", methods=["GET"])
 def get_relays():
-    return api.get_relays(relays)
+    return api.get_relays(relays_dict)
 
 
 @server.route("/relay-api/relays/<relay_name>", methods=["GET"])
 def get_relay(relay_name):
-    return api.get_relay(relays, relay_name)
+    return api.get_relay(relays_dict.get(relay_name, "None"))
