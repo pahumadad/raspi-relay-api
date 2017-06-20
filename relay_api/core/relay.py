c17fed815cd062b37ebe5e6118da43afcf89db1f
Pablo Ahumada <pablo.ahumadadiaz@gmail.com>
2017-06-20 05:28:20 +0000

    Add nc and state as attributes. Change name of gpio_num to gpio

--- relay_api/core/relay.py
+++ relay_api/core/relay.py
@@ -3,28 +3,31 @@ import RPi.GPIO as GPIO
 
 class relay():
     def __init__(self, gpio_num, NC=False):
-        self.gpio_num = gpio_num
+        self.gpio = gpio_num
+        self.nc = NC
         GPIO.setmode(GPIO.BCM)
         try:
-            GPIO.input(self.gpio_num)
+            GPIO.input(self.gpio)
             raise LookupError("Relay is already in use!")
         except RuntimeError:
-            GPIO.setup(self.gpio_num, GPIO.OUT)
+            GPIO.setup(self.gpio, GPIO.OUT)
         except ValueError:
             raise LookupError("Relay number invalid!")
-        if NC:
+        if self.nc:
             self.on()
         else:
             self.off()
 
     def on(self):
-        GPIO.output(self.gpio_num, GPIO.HIGH)
+        GPIO.output(self.gpio, GPIO.HIGH)
+        self.state = True
 
     def off(self):
-        GPIO.output(self.gpio_num, GPIO.LOW)
+        GPIO.output(self.gpio, GPIO.LOW)
+        self.state = False
 
     def get_state(self):
-        return GPIO.input(self.gpio_num)
+        return self.state
 
     def cleanup(self):
-        GPIO.cleanup(self.gpio_num)
+        GPIO.cleanup(self.gpio)
