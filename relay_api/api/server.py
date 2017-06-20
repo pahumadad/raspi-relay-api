e1c8dcd56e64f3047e3d3823afe323b93d3e6af7
Pablo Ahumada <pablo.ahumadadiaz@gmail.com>
2017-06-19 22:51:53 -0400

    Modify commit 51c50eaaf2e7374f3d6797b82df9604126f6000d
    Update to handle a dict of dicts instead a list of dicts

--- relay_api/api/server.py
+++ relay_api/api/server.py
@@ -1,14 +1,18 @@
-from flask import Flask, jsonify, abort
+from flask import Flask, jsonify
 
 server = Flask(__name__)
 
 
 def get_relays(relays):
-    return jsonify({"relays": relays})
+    return jsonify({"relays": relays}), 200
 
 
-def get_relay(relays, relay_id):
-    relay = [relay for relay in relays if relay["id"] == relay_id]
-    if len(relay) == 0:
-        abort(404)
-    return jsonify({"relay": relay[0]})
+def get_relay(relays, relay_name):
+    code = 200
+    try:
+        relay = relays[relay_name]
+    except KeyError:
+        code = 404
+        return "", code
+
+    return jsonify({"relay": relay}), code
