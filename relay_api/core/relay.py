383be458a4e38a7ee36a9f568f94f8af10939cb2
Pablo Ahumada <pablo.ahumadadiaz@gmail.com>
2017-06-24 00:47:24 -0400

    Eliminate NC argument and attribute

--- relay_api/core/relay.py
+++ relay_api/core/relay.py
@@ -2,9 +2,8 @@ import RPi.GPIO as GPIO
 
 
 class relay():
-    def __init__(self, gpio_num, NC=False):
+    def __init__(self, gpio_num):
         self.gpio = gpio_num
-        self.nc = NC
         GPIO.setmode(GPIO.BCM)
         try:
             GPIO.input(self.gpio)
@@ -13,10 +12,7 @@ class relay():
             GPIO.setup(self.gpio, GPIO.OUT)
         except ValueError:
             raise LookupError("Relay number invalid!")
-        if self.nc:
-            self.on()
-        else:
-            self.off()
+        self.off()
 
     def on(self):
         GPIO.output(self.gpio, GPIO.HIGH)
