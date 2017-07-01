da98272c3b19828dabbbb339f025c9d3dd4a949e
Pablo Ahumada <pablo.ahumadadiaz@gmail.com>
2017-07-01 16:36:46 -0400

    Change the way that GPIO is verified

--- relay_api/core/relay.py
+++ relay_api/core/relay.py
@@ -1,17 +1,23 @@
 import RPi.GPIO as GPIO
 
+MAX_RELAY_GPIO = 27
+
 
 class relay():
     def __init__(self, gpio_num):
+        if gpio_num not in range(MAX_RELAY_GPIO + 1):
+            raise LookupError("Relay GPIO invalid! Use one between 0 - " +
+                              str(MAX_RELAY_GPIO))
+
         self.gpio = gpio_num
         GPIO.setmode(GPIO.BCM)
+
         try:
             GPIO.input(self.gpio)
-            raise LookupError("Relay is already in use!")
+            raise LookupError("Relay GPIO is already in use!")
         except RuntimeError:
             GPIO.setup(self.gpio, GPIO.OUT)
-        except ValueError:
-            raise LookupError("Relay number invalid!")
+
         self.off()
 
     def on(self):
